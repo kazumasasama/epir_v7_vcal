a2748ab681f15d02d3a8ae74ab224c21ff2857c1
kazumasasama <72055437+kazumasasama@users.noreply.github.com>
2021-10-08 13:44:18 -0400

    カレンダー設定追加

--- src/main.js
+++ src/main.js
@@ -14,20 +14,6 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
 
-import { Calendar } from "@fullcalendar/core";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-
-document.addEventListener("turbolinks:load", function () {
-  var calendarEl = document.getElementById("calendar");
-
-  var calendar = new Calendar(calendarEl, {
-    plugins: [timeGridPlugin, interactionPlugin],
-  });
-
-  calendar.render();
-});
-
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <h1>予約カレンダー</h1>
-    <button @click="toggleWeekends">toggle weekends</button>
     <FullCalendar :options="calendarOptions" />
   </div>
 </template>
@@ -10,8 +9,9 @@
 import "@fullcalendar/core/vdom"; // solves problem with Vite
 import FullCalendar from "@fullcalendar/vue";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from '@fullcalendar/timegrid';
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import jaLocale from "@fullcalendar/core/locales/ja";
 
 export default {
   components: {
@@ -23,14 +23,65 @@ export default {
         plugins: [timeGridPlugin, dayGridPlugin, interactionPlugin],
         initialView: "timeGridWeek",
         dateClick: this.handleDateClick,
-        weekends: false, // initial value
+        nowIndicator: true,
+        locales: jaLocale,
+        locale: "ja",
+        timeZone: "Asia/Tokyo",
+        slotDuration: "00:30:00",
+        scrollTime: "10:00:00",
+        titleFormat: { year: "numeric", month: "2-digit" },
+        selectable: true,
+        editable: true,
+        eventOverlap: false,
+        events: [
+          {
+            hour: "numeric",
+            minute: "2-digit",
+            meridiem: false,
+          },
+          {
+            title: "にゃんこ",
+            start: "2021-10-07T16:00:00",
+            end: "2021-10-07T19:00",
+            color: "black",
+          },
+          {
+            title: "わんこ",
+            start: "2021-10-08T16:00:00",
+            end: "2021-10-08T19:00",
+          },
+        ],
+        businessHours: {
+          // days of week. an array of zero-based day of week integers (0=Sunday)
+          daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
+
+          startTime: "10:00",
+          endTime: "20:00",
+        },
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek",
+        },
+        views: {
+          dayGridMonth: {
+            headerToolbar: {
+              start: "",
+              center: "prev today next",
+              end: "dayGridMonth,timeGridWeek",
+            },
+          },
+          timeGridWeek: {
+            headerToolbar: {
+              center: "prev today next",
+              end: "dayGridMonth,timeGridWeek",
+            },
+          },
+        },
       },
     };
   },
   methods: {
-    toggleWeekends: function () {
-      this.calendarOptions.weekends = !this.calendarOptions.weekends; // toggle the boolean!
-    },
     handleDateClick: function (arg) {
       alert("date click! " + arg.dateStr);
     },
